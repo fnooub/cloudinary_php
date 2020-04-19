c15e2b081addf50d71dd3411fd52ccd9d5c1667f
fnooub <61742373+fnooub@users.noreply.github.com>
2020-04-19 18:00:08 +0700

    test

---
+++ upload.php
@@ -0,0 +1,4 @@
+<form action="upload_backend.php" method="post" enctype="multipart/form-data">
+	<input id="fileupload" type="file" name="files[]" multiple accept="image/gif, image/jpeg, image/png">
+	<input type="submit" value="Upload">
+</form>
